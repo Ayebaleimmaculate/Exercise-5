c8fb5a0fdea33d5a1b07d622db759bdfca414baf
Ayebaleimmaculate <142968605+Ayebaleimmaculate@users.noreply.github.com>
2023-10-11 04:10:04 -0700

    EXERCISE

---
+++ functions.py
@@ -0,0 +1,22 @@
+#write a python function to reverse a string "1,2,3,4,a,b,c,d"
+def mystring(str):
+    str = "1,2,3,4,a,b,c,d"
+    reversed_string =(str)
+    for p in reversed(str):
+        print(p)
+mystring("1,2,3,4,a,b,c,d")
+
+
+#write a python function to multiply all the numbers in a list 
+#list =[8,2,3,-1,7]
+def product_of_all_numbers(m,n,o,p,q):
+    output =[m*n*o*p*q]
+    print(output)
+product_of_all_numbers(8,2,3,-1,7)
+
+
+#write a python program to print the even numbers from a given list
+def even_numbers_in_list(h,i,j,k,l,m,n,s,t,x):
+    even_list=[i,k,m,s,x]
+    print(even_list)
+even_numbers_in_list(1,2,3,4,5,6,7,8,9,10)
